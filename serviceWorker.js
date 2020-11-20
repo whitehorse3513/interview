b316be2c01cebbda683d963c83d87f5747a057ec
whitehorse3513 <48043357+whitehorse3513@users.noreply.github.com>
2020-11-20 20:57:41 +0100

    first update

---
+++ serviceWorker.js
@@ -0,0 +1,33 @@
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/admin.html",
+  "/interview.html",
+  "/real_interview.html",
+  "/test_done.html",
+  "/test_no.html",
+  "/test_yes.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/1.png",
+  "/images/loading.gif",
+  "/sounds/error.mp3",
+  "/sounds/shutter.mp3",
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
